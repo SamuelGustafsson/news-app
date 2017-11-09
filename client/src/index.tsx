47c20ca652311a3a67d1696330511a675c07cc29
SamuelGustafsson <samuelgustafsson890623@gmail.com>
2017-11-09 22:41:44 +0100

    Redux and redux-devtools added.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,14 +2,19 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import RootContainer from "./root-container";
+import { Provider } from "react-redux";
+import { store } from "./store";
 import baseStyles from "./styles/index";
 
 const render = () => {
   baseStyles();
 
-  ReactDOM.render(<RootContainer />, document.getElementById(
-    "root"
-  ) as HTMLElement);
+  ReactDOM.render(
+    <Provider store={store}>
+      <RootContainer />
+    </Provider>,
+    document.getElementById("root") as HTMLElement
+  );
   registerServiceWorker();
 };
 
